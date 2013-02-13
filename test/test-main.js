289f2212dedbe559447d16733af95dc3574a326f
David Keeler <dkeeler@mozilla.com>
2013-02-13 15:13:16 -0800

    initial implementation of bloom filter

--- test/test-main.js
+++ test/test-main.js
@@ -1,12 +1,17 @@
-var main = require("main");
+let main = require("main");
+let bpUtil = require("bpUtil");
 
 exports["test main"] = function(assert) {
-  assert.pass("Unit test running!");
-};
-
-exports["test main async"] = function(assert, done) {
-  assert.pass("async Unit test running!");
-  done();
+  let filter = new ArrayBuffer(1024);
+  assert.ok(!bpUtil.probeBloomFilter(filter, "asdf"), "'asdf' shouldn't be in yet");
+  bpUtil.addToBloomFilter(filter, "asdf");
+  assert.ok(bpUtil.probeBloomFilter(filter, "asdf"), "'asdf' should be in now");
+  assert.ok(!bpUtil.probeBloomFilter(filter, "fsda"), "'fsda' shouldn't be in yet");
+  bpUtil.addToBloomFilter(filter, "fsda");
+  bpUtil.addToBloomFilter(filter, "anatidaeAnseriformes");
+  bpUtil.addToBloomFilter(filter, "Donaudampfschiffahrtsgesellschaftskapitän");
+  assert.ok(bpUtil.probeBloomFilter(filter, "asdf"), "'asdf' should still be in");
+  assert.ok(bpUtil.probeBloomFilter(filter, "fsda"), "'fsda' should be in now");
 };
 
 require("test").run(exports);
