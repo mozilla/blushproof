a43f56d9c0e1b723799dd6bc4b8f9e686bfb536d
David Keeler <dkeeler@mozilla.com>
2013-02-13 15:43:59 -0800

    more tests, less console.log

--- test/test-main.js
+++ test/test-main.js
@@ -12,6 +12,13 @@ exports["test main"] = function(assert) {
   bpUtil.addToBloomFilter(filter, "Donaudampfschiffahrtsgesellschaftskapitän");
   assert.ok(bpUtil.probeBloomFilter(filter, "asdf"), "'asdf' should still be in");
   assert.ok(bpUtil.probeBloomFilter(filter, "fsda"), "'fsda' should be in now");
+
+  for (let i = 0; i < 778; i++) {
+    assert.ok(!bpUtil.probeBloomFilter(filter, String(i)), i + " not yet in");
+    bpUtil.addToBloomFilter(filter, String(i));
+    assert.ok(bpUtil.probeBloomFilter(filter, String(i)), i + " now in");
+  }
+  assert.ok(bpUtil.probeBloomFilter(filter, "778"), "we get a false positive at 778");
 };
 
 require("test").run(exports);
