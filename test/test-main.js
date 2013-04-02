4e4edb08ab0cfd7c2bd9c3d0c99cab4d88eb54c9
David Keeler <dkeeler@mozilla.com>
2013-04-02 16:20:09 -0700

    refactor test framework

--- test/test-main.js
+++ test/test-main.js
@@ -25,43 +25,112 @@ function runNextTest() {
 }
 
 /**
+ * Given a url and a continuation to call upon completion, this function loads
+ * up the url and expects that the consent panel will be shown.
+ * @param aURL a string representing a url to load
+ * @param aContinuation a function to call upon completion. The function takes
+ *        two arguments: a boolean indicating success if true, and the
+ *        event spawned by either the page load or consent panel opening.
+ */
+function expectConsentPanel(aURL, aContinuation) {
+  expectOrNotPanelCommon(aURL, aContinuation, true, true);
+}
+
+/**
+ * Same as expectConsentPanel, except it is expected that the page load
+ * succeeds and that no consent panel is shown.
+ */
+function expectNoConsentPanel(aURL, aContinuation) {
+  expectOrNotPanelCommon(aURL, aContinuation, false, true);
+}
+
+/**
+ * Same as expectNoConsentPanel, except it only sets up the appropriate
+ * listeners - no navigation is done. The caller of this function must
+ * perform the navigation itself (e.g. by clicking "open anyway" in a
+ * previously shown consent panel).
+ */
+function expectNoConsentPanelNoNav(aURL, aContinuation) {
+  expectOrNotPanelCommon(aURL, aContinuation, false, false);
+}
+
+/**
+ * Common function for expectConsentPanel, expectNoConsentPanel, and
+ * expectNoConsentPanelNoNav. Probably shouldn't be called directly (use
+ * one of the aforementioned functions).
+ * @param aURL same as previously mentioned functions
+ * @param aContinuation same as previously mentioned functions
+ * @param aExpectPanel a boolean indicating the panel is expected if true
+ * @param aDoNav a boolean indicating the page should be navigated if true
+ */
+function expectOrNotPanelCommon(aURL, aContinuation, aExpectPanel, aDoNav) {
+  let win = winUtils.getMostRecentBrowserWindow();
+  let currentBrowser = win.gBrowser.selectedBrowser;
+
+  let loadListener = null;
+  let consentPanelShownListener = null;
+  let current = true;
+
+  loadListener = function(event) {
+    if (event.target.documentURI == aURL) {
+      currentBrowser.removeEventListener("load", loadListener);
+      win.removeEventListener("ConsentPanelShown", consentPanelShownListener);
+      if (!current) {
+        return;
+      }
+      if (aExpectPanel) {
+        gAssertObject.fail("got page load when we shouldn't have");
+      } else {
+        gAssertObject.pass("got page load when it was okay to");
+      }
+      current = false;
+      aContinuation(!aExpectPanel, event);
+    }
+  };
+
+  consentPanelShownListener = function(event) {
+    currentBrowser.removeEventListener("load", loadListener);
+    win.removeEventListener("ConsentPanelShown", consentPanelShownListener);
+    if (!current) {
+      return;
+    }
+    if (aExpectPanel) {
+      gAssertObject.pass("panel shown when we were expecting it");
+    } else {
+      gAssertObject.fail("panel shown when we weren't expecting it");
+    }
+    current = false;
+    aContinuation(aExpectPanel, event);
+  };
+
+  currentBrowser.addEventListener("load", loadListener, true);
+  win.addEventListener("ConsentPanelShown", consentPanelShownListener);
+
+  // Using 'currentBrowser.contentWindow.location = aURL;'
+  // somehow bypasses our content policy. I'm guessing it has something to do
+  // with chrome privileges, but I'm too annoyed with it to figure it out now.
+  if (aDoNav) {
+    tabs[0].url = aURL;
+  }
+}
+
+/**
  * The first time we load localhost:4444, we expect to see a consent
  * panel, because it's on the blushlist (we added it for this test under
  * the category "testing").
  */
 function testExpectConsentPanel() {
-  let win = winUtils.getMostRecentBrowserWindow();
-  let currentBrowser = win.gBrowser.selectedBrowser;
-  let okayToLoad = false;
-  currentBrowser.addEventListener("load",
-    function loadListener(event) {
-      if (event.target.documentURI == "http://localhost:4444/") {
-        if (!okayToLoad) {
-          gAssertObject.fail("got page load when shouldn't have...");
-        } else {
-          gAssertObject.pass("got page load when it was okay to");
-          currentBrowser.removeEventListener("load", loadListener);
-          runNextTest();
-        }
+  expectConsentPanel("http://localhost:4444/",
+    function(aSuccess, aEvent) {
+      if (aSuccess) {
+        let panel = aEvent.detail;
+        expectNoConsentPanelNoNav("http://localhost:4444/", runNextTest);
+        panel.postMessage("continue");
+      } else {
+        runNextTest();
       }
-    },
-    true
-  );
-  win.addEventListener("ConsentPanelShown",
-    function consentPanelShownListener(event) {
-      win.removeEventListener("ConsentPanelShown", consentPanelShownListener);
-      gAssertObject.pass("panel shown");
-      okayToLoad = true;
-      let panel = event.detail;
-      panel.postMessage("continue");
     }
   );
-
-  // now actually do the navigation
-  tabs[0].url = "http://localhost:4444/";
-  // Using 'currentBrowser.contentWindow.location = "http://localhost:4444/";'
-  // somehow bypasses our content policy. I'm guessing it has something to do
-  // with chrome privileges, but I'm too annoyed with it to figure it out now.
 }
 
 /**
@@ -69,29 +138,7 @@ function testExpectConsentPanel() {
  * panel, because we whitelisted it in the previous test.
  */
 function testExpectNoConsentPanelWhitelisted() {
-  let win = winUtils.getMostRecentBrowserWindow();
-  let currentBrowser = win.gBrowser.selectedBrowser;
-  currentBrowser.addEventListener("load",
-    function loadListener(event) {
-      if (event.target.documentURI == "http://localhost:4444/") {
-        gAssertObject.pass("got page load when it was okay to");
-        currentBrowser.removeEventListener("load", loadListener);
-        runNextTest();
-      }
-    },
-    true
-  );
-  win.addEventListener("ConsentPanelShown",
-    function consentPanelShownListener(event) {
-      win.removeEventListener("ConsentPanelShown", consentPanelShownListener);
-      gAssertObject.fail("panel shown when we weren't expecting it");
-      let panel = event.detail;
-      panel.postMessage("continue");
-    }
-  );
-
-  // now actually do the navigation
-  tabs[0].url = "http://localhost:4444/";
+  expectNoConsentPanel("http://localhost:4444/", runNextTest);
 }
 
 /**
@@ -107,8 +154,7 @@ function testExpectNoConsentPanelNotOnBlushlist() {
                       "'localhost' should not be on the blushlist");
   gAssertObject.ok(!ss.storage.whitelistedDomains["localhost"],
                    "'localhost' should not be on the domain whitelist");
-  // just sharing code here
-  testExpectNoConsentPanelWhitelisted();
+  expectNoConsentPanel("http://localhost:4444/", runNextTest);
 }
 
 function finishTest() {
