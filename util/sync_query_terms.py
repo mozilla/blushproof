e91b22d00d29b760895b307a623cc982160abec2
Monica Chew <mmc@mozilla.com>
2013-05-02 13:10:46 -0700

    Trim top_adult_term_hits.txt for false positives

--- util/sync_query_terms.py
+++ util/sync_query_terms.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python
-# Given a list of addon ids, return a verdict of bad or not for each id.
-# Usage: baddons.py <input_file> <output_file>
+"""A class to collect the top N hits for a list of query terms."""
+# Usage: sync_query_terms.py <input_file> <output_file>
 
-import csv
-import logging
 import json
 import pycurl
-import re
 import sys
 import tldextract
 
@@ -16,21 +13,21 @@ class QuerySyncer:
   """A class to collect the top N hits for a list of query terms."""
 
   def __init__(self, apikey, max_count=5):
-    self.APIKEY = apikey
-    self.MAX_COUNT = max_count
+    self.api_key = apikey
+    self.max_count = max_count
 
   def get_top_hits(self, search_term):
     """Returns 1 if the addon_id is probably bad, 0 otherwise."""
     # A custom search URL. cx is a global search engine.
     search_url = ("https://www.googleapis.com/customsearch/v1?" +
             "cx=018149516584340204128:67tqllu_gne&key=%s&q=%s&alt=json" %
-            (self.APIKEY, search_term))
+            (self.api_key, search_term))
     buf = StringIO.StringIO()
-    c = pycurl.Curl()
-    c.setopt(c.URL, search_url)
-    c.setopt(c.WRITEFUNCTION, buf.write)
-    c.perform()
-    c.close()
+    curl = pycurl.Curl()
+    curl.setopt(curl.URL, search_url)
+    curl.setopt(curl.WRITEFUNCTION, buf.write)
+    curl.perform()
+    curl.close()
     return self.parse_results(buf.getvalue())
 
   def parse_results(self, result_string):
@@ -41,7 +38,7 @@ class QuerySyncer:
     urls = []
     count = 0
     for i in result["items"]:
-      if (count == self.MAX_COUNT):
+      if (count == self.max_count):
         break
       if i["kind"] != "customsearch#result":
         raise Exception("Unexpected results", result_string)
@@ -56,8 +53,8 @@ class QuerySyncer:
     # Each line of the input contains an addon id
     try:
       f_in = open(input_file)
-    except IOError as e:
-      sys.exit("Can't find file: %s" % e)
+    except IOError as error:
+      sys.exit("Can't find file: %s" % error)
     for line in f_in.readlines():
       search_term = line.strip()
       urls = self.get_top_hits(search_term)
@@ -68,12 +65,13 @@ class QuerySyncer:
 
 
 def main():
+  """Run it."""
   if len(sys.argv) < 3:
     sys.exit("Usage: sync_query_terms.py <input_file> <output_file>")
   try:
     f_api = open(".apikey", "r")
-  except IOError as e:
-    sys.exit("Can't find apikey: %s" % e)
+  except IOError as error:
+    sys.exit("Can't find apikey: %s" % error)
   apikey = f_api.readline().strip()
   syncer = QuerySyncer(apikey)
   syncer.process_search_terms(sys.argv[1], sys.argv[2])
