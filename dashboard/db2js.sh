2d6705800d644e45d09cfee983478add663569af
David Keeler <mozkeeler@gmail.com>
2013-06-11 15:09:47 -0700

    make db2js.sh take a path to events.sqlite

--- dashboard/db2js.sh
+++ dashboard/db2js.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]; then
+  echo "Usage: `basename ${0}` <path to events.sqlite>"
+  exit $E_BADARGS
+fi
+
+EVENTS_SQLITE=${1}
+if [ ! -f "$EVENTS_SQLITE" ]; then
+  echo "Could not find events database at '$EVENTS_SQLITE'"
+  exit $E_BADARGS
+fi
+
 echo "var events = ["
-for line in `echo "" | sqlite3 -cmd 'select eventTS,eventSTR from events;' events.sqlite`; do
+for line in `echo "" | sqlite3 -cmd 'select eventTS,eventSTR from events;' $EVENTS_SQLITE`; do
   event=`echo $line | cut -d "|" -f 2`
   ts=`echo $line | cut -d "|" -f 1`
   echo "  { event: \"$event\", time: $ts },"
